e6f5ddcdb5357f4f26b77238539e2021162a3c53
William Hoang <46702069+Consolefun@users.noreply.github.com>
2019-09-14 18:20:31 -0500

    Second commit
    updated hover.js

--- Web_Development/Web_ICP4/Hover_Image/hover.js
+++ Web_Development/Web_ICP4/Hover_Image/hover.js
@@ -7,21 +7,26 @@ function upDate(previewPic) {
        2) Change the text  of the div with the id = "image"
        to the alt text of the preview image
        */
-    var src = previewPic.getAttribute("src");
+
     var myAlt = previewPic.getAttribute("alt");
 
-    // Check if alt text is match then update background image and it's alt text
+    var target = document.getElementById("image");
+
+
     if(myAlt == "Batter is ready"){
-        document.getElementById("image").style.backgroundImage = "url('"+src+"')";
-        document.getElementById('image').innerHTML = myAlt;
+        target.style.backgroundImage = "url('https://cdn.sallysbakingaddiction.com/wp-content/uploads/2017/06/moist-chocolate-cupcakes-7.jpg')";
+        target.innerHTML = myAlt;
+
     }
     else if(myAlt == "Perfect Baking"){
-        document.getElementById("image").style.backgroundImage = "url('"+src+"')";
-        document.getElementById('image').innerHTML = myAlt;
+        target.style.backgroundImage = "url('https://cdn.sallysbakingaddiction.com/wp-content/uploads/2017/06/moist-chocolate-cupcakes-6.jpg')";
+        target.innerHTML = myAlt;
+
     }
     else if(myAlt == "Yummy yummy cup cake"){
-        document.getElementById("image").style.backgroundImage = "url('"+src+"')";
-        document.getElementById('image').innerHTML = myAlt;
+        target.style.backgroundImage = "url('https://cdn.sallysbakingaddiction.com/wp-content/uploads/2017/06/moist-chocolate-cupcakes-5.jpg')";
+        target.innerHTML = myAlt;
+
     }
 
 
@@ -36,10 +41,9 @@ function unDo() {
    2) Change the text  of the div with the id = "image"
    back to the original text.  You can use the html code to see what that original text was
    */
-
-    // Set the image back to it's original image. Div with id image
     original_img = document.getElementById('image');
     original_img.style.backgroundImage = "url('')";
     var target = document.getElementById("image");
     target.innerText = "Hover over an image below to display here.";
+
 }
